b66ba79833395e7ef916690af0cfffbbc6c45de8
bryanguillen <bryan.guillen217@gmail.com>
2024-01-30 17:23:09 -0500

    Implement support for demoing dynamic env vars

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.tsx'
+import environmentVariables, {
+  getEnvironmentVariablesJson,
+} from './utils/environmentVariables'
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const renderComponentInRoot = (reactNode: React.ReactNode) => {
+  return ReactDOM.createRoot(document.getElementById('root')!).render(reactNode)
+}
+
+const startApp = async () => {
+  try {
+    const environmentVariablesJson = await getEnvironmentVariablesJson()
+    environmentVariables.set(environmentVariablesJson)
+
+    renderComponentInRoot(<App />)
+  } catch (error) {
+    renderComponentInRoot(
+      <div>Oops! An error occurred while trying to load the application</div>
+    )
+  }
+}
+
+startApp()
